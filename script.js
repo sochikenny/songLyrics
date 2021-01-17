f164bf3607a69b1fb200c339e9ab7d2a553d83b5
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-01-17 11:49:05 -0500

    finish project

--- script.js
+++ script.js
@@ -10,53 +10,35 @@ const apiURL = 'https://api.lyrics.ovh';
 async function searchSongs(term){
     const response = await fetch(`${apiURL}/suggest/${term}`)
     const data = await response.json()
-    
+    console.log(data)
     //showData is a function for updating the UI/DOM
     showData(data)
 }
 
-//Show song and artist in DOM (function)
-function showData(data){
-    // let output = ''
 
-    // data.data.forEach(song=>{
-    //     output += `
-    //     <li>
-    //     <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-    //     <button class="btn" data-artist="${song.artist.name}"
-    //     data-songtitle="${song.title}">Get Lyrics</button>
-    //     </li>
-    //     `;
-    // });
-
-    // result.innerHTML = `
-    // <ul class="songs">
-    // ${output}
-    // </ul>
-    // `;
-
+// Show song and artist in DOM (basically update the UI)
+function showData(data) {
     result.innerHTML = `
-    <ul class="songs">
-    ${data.data
-        .map(song => 
-    `<li>
+      <ul class="songs">
+        ${data.data.map(song => 
+        `<li>
         <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-        <button class="btn" data-artist="${song.artist.name}"
-        data-songtitle="${song.title}">Get Lyrics</button>
-    </li>`
-    ).join('')}
-    </ul>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>`)
+        .join('')}
+      </ul>
     `;
-
-    if(data.prev || data.next){
-        more.innerHTML = `
-        ${data.prev ? `<button class="btn" onClick="getMoreSongs('${data.prev}')">Prev</button>` : ''}
-        ${data.next ? `<button class="btn" onClick="getMoreSongs('${data.next}')">Next</button>` : ''}
-        `;
+  
+    if (data.prev || data.next) {
+      more.innerHTML = `
+        ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`: ''}
+        ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`: ''}
+      `;
     }else{
         more.innerHTML = '';
     }
 }
+
 
 //function that uses pagination to get more songs(basically going from page to page)
 async function getMoreSongs(url){
@@ -64,6 +46,20 @@ async function getMoreSongs(url){
     const data = await res.json();
 
     showData(data);
+}
+    
+
+//Get Lyrics for song (function)
+async function getLyrics(artist, songTitle){
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+    
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}<strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>`;
+    
+    more.innerHTML = '';
 }
 
 
@@ -81,3 +77,19 @@ form.addEventListener('submit', e => {
     }
 
 });
+
+
+//Get lyrics button click
+result.addEventListener('click', e => {
+    const clickedEl = e.target; 
+
+    if(clickedEl.tagName === 'BUTTON'){
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
+
+        getLyrics(artist, songTitle)
+    }
+})
+
+
+  
